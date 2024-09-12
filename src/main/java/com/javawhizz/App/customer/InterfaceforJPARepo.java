a1a8ec67053d53d784ef3a0d95446d49e8020372
Ismail948 <120742683+Ismail948@users.noreply.github.com>
2024-09-12 18:44:04 +0530

    Delete src directory

--- src/main/java/com/javawhizz/App/customer/InterfaceforJPARepo.java
+++
@@ -1,7 +0,0 @@
-package com.javawhizz.App.customer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InterfaceforJPARepo extends JpaRepository<Account,Long>{//Account is Entity and Long is the datatype of Primary key
-
-}
